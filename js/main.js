512f15fc5bf0cbc3d56fff5479b5440e6029ebdf
FranMMen <110855356+FranchoM@users.noreply.github.com>
2022-10-29 13:54:39 -0300

    testeo de ejecucion

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 const btnRedireccion1 = document.getElementById("compraTickets1");
 const btnRedireccion2 = document.getElementById("compraTickets2");
+const btnEnvio = document.getElementById("envioForm");
 
 function redireccionamiento(){
     window.location = "pages/compraTickets.html";
 }
 
+function envioFormulario(){
+    alert("¡Gracias por tu ofrecimiento!\nSin duda te tendremos en cuenta para nuestra próxima conferencia")
+}
+
 btnRedireccion1.addEventListener("click", redireccionamiento);
 btnRedireccion2.addEventListener("click", redireccionamiento);
+btnEnvio.addEventListener("click", envioFormulario);
